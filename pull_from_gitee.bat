06f915ef1d1d0cfdd56399ef50029d1c9d68c300
luopub <22909077@qq.com>
2022-04-06 16:22:15 +0800

    add batches

---
+++ pull_from_gitee.bat
@@ -0,0 +1,3 @@
+git config credential.helper store
+git pull https://gitee.com/luopub/capture_aiqicha.git master
+pause
---
+++ push_both.bat
@@ -0,0 +1,3 @@
+git config credential.helper store
+git push https://gitee.com/luopub/nc19spiders.git master
+git push https://github.com/luopub/nc19spiders.git master
